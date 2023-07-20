3ec182512a7c8c51c720490bae137a80960bc1ab
kociumba <kociumba1@gmail.com>
2023-07-20 18:56:53 +0200

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,181 @@
+<script>
+    // @ts-ignore
+    import { round } from 'lodash';
+    
+    let price = NaN;
+    let discount = NaN;
+    let full_price = 0;
+    let discount_amount = 0;
+    let full_price_no_tax = 0;
+    let tax = 0;
+    let country = "";
+    let is_it_scam = "";
+    
+    $: {
+        console.log(country);
+        calculate_button_press();
+    }
+    
+    export function calculate_button_press() {
+        switch (country) {
+            case "Poland":
+            tax = 0.23;
+            break;
+            case "Australia":
+            tax = 0.10;
+            break;
+            case "Canada":
+            tax = 0.05;
+            break;
+            case "Egypt":
+            tax = 0.14;
+            break;
+            case "Russia":
+            tax = 0.20;
+            break;
+            case "Germany":
+            tax = 0.19;
+            break;
+            case "Japan":
+            tax = 0.1;
+            break;
+            case "Mexico":
+            tax = 0.16;
+            break;
+            case "Ukraine":
+            tax = 0.20;
+            break;
+            case "USA":
+            tax = 0.05;
+            break;
+            case "United Arab Emirates":
+            tax = 0.05;
+            break;
+            case "no tax":
+            tax = 0;
+            break;
+        }
+        
+        let parsedDiscount = discount / 100;
+        console.log(parsedDiscount);
+        discount_amount = parsedDiscount * price;
+        full_price_no_tax = price - discount_amount;
+        console.log(full_price_no_tax);
+        full_price = full_price_no_tax + full_price_no_tax * tax;
+        console.log(full_price);
+
+        if (full_price > price) {
+            is_it_scam = "the government is scamming you";
+        } else {
+            is_it_scam = "";
+        }
+    }
+</script>
+
+
+<div class="container">
+    <div class="secondary_containers">
+        <h1>price</h1>
+        <input type="number" placeholder="price of the item" bind:value={price}>
+    </div>
+    <div class="secondary_containers">
+        <h1>discount</h1>
+        <input type="number" placeholder="discount on the thing" max="100" bind:value={discount}>
+    </div>
+    <div class="secondary_containers">
+        <h1>country</h1>
+        <form id="form">
+            <label for="form"></label>
+            <select bind:value={country}>
+                <option value="Poland">Poland</option>
+                <option value="Australia">Australia</option>
+                <option value="Canada">Canada</option>
+                <option value="Egypt">Egypt</option>
+                <option value="Russia">Russia</option>
+                <option value="Japan">Japan</option>
+                <option value="Mexico">Mexico</option>
+                <option value="Ukraine">Ukraine</option>
+                <option value="USA">USA</option>
+                <option value="United Arab Emirates">United Arab Emirates</option>
+                <option value="no tax">no tax</option>
+            </select>
+        </form>
+    </div>
+    <div class="secondary_containers">
+        <button on:click={calculate_button_press}>
+            <h1 class="btn_text">Calculate</h1>
+        </button>
+    </div>
+    <div class="secondary_containers">
+        <h1>{round(full_price, 2)}</h1>
+        <h2>{is_it_scam}</h2>
+    </div>
+</div>
+
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap');
+    
+    :global()::selection {
+        background-color: #cc00ff;
+        color: #020104;
+    }
+    
+    :global(body) {
+        background: #020104;
+        background: radial-gradient(50% 50%, ellipse cover, #CC00FF 0, #020104 100%);
+    }
+    
+    .container {
+        display: grid;
+        place-items: center;
+        font-family: Poppins;
+        text-transform: capitalize;
+    }
+    
+    h1 {
+        color: #fafafa; 
+        padding: 5px;
+    }
+    
+    button {
+        background-color: #fafafa;
+        border-radius: 20px;
+        border: #020104 1px solid;
+        cursor: pointer;
+        transition: 300ms;
+    }
+    
+    button:hover {
+        border-radius: 15px;
+        border: #ffffff 1px solid;
+    }
+    
+    .btn_text {
+        color: #020104;
+    }
+    
+    .secondary_containers {
+        display: grid;
+        place-items: center;
+        /*width: 100%;*/
+        margin: 20px;
+    }
+    
+    select {
+        width: 300px;
+        height: 20px;
+        padding: 0px;
+        border: 5px;
+        border-radius: 1px;
+    }
+    
+    input {
+        width: 300px;
+        height: 20px;
+        padding: 0px;
+        border: 5px;
+        border-radius: 1px;
+    }
+</style>
+
